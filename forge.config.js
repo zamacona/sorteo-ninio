f916361805ae94d42b3045657cd3b6baf4125619
Carlos Eymard <cezamacona@gmail.com>
2023-04-28 10:54:11 -0600

    electron make windwos

--- forge.config.js
+++ forge.config.js
@@ -4,7 +4,10 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        description: "Tombola.",
+        authors: "Carlos Zamacona Lopez",
+      },
     },
     {
       name: '@electron-forge/maker-zip',
